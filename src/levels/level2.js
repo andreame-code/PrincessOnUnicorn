3dfd369f0f35e3c2f545de51db2f75b2b875c732
andreame-code <andrea.pasqualini.me@gmail.com>
2025-08-24 01:19:00 +0200

    refactor: introduce base level class

--- src/levels/level2.js
+++ src/levels/level2.js
@@ -1,13 +1,11 @@
+import { BaseLevel } from './baseLevel.js';
 import { Obstacle } from '../obstacle.js';
 import { isColliding } from '../../collision.js';
 
-export class Level2 {
+export class Level2 extends BaseLevel {
   constructor(game, random = Math.random) {
-    this.game = game;
-    this.random = random;
-    this.walls = [];
-    this.wallTimer = 0;
-    this.wallInterval = 90 / 60; // seconds
+    super(game, random);
+    this.interval = 90 / 60; // seconds
     this.boss = { x: game.canvas.width - 100, y: game.groundY, width: 40, height: 50 };
     this.bossFlee = false;
     this.coins = [];
@@ -15,36 +13,51 @@ export class Level2 {
       this.boss.x - (this.game.player.x + this.game.player.width);
   }
 
-  update(delta) {
-    const speed = this.game.speed + 120; // px per second
-    const move = speed * delta;
-    this.wallTimer += delta;
-    if (this.wallTimer > this.wallInterval && !this.bossFlee) {
-      this.walls.push(new Obstacle(this.boss.x, this.game.groundY, 30, 50));
-      this.wallTimer = 0;
-      this.wallInterval = (60 + this.random() * 60) / 60;
-    }
+  static getInterval(random) {
+    return (60 + random() * 60) / 60;
+  }
 
-    this.walls.forEach(w => w.update(move));
-    this.walls = this.walls.filter(w => {
-      if (w.x + w.width < 0) return false;
-      if (isColliding(this.game.player, w)) {
-        if (this.game.player.shieldActive) {
-          this.game.player.x += 20;
-          this.coins.push({
-            x: w.x + w.width / 2,
-            y: w.y - w.height / 2,
-            vy: -120,
-            life: 0.5
-          });
-          this.game.coins++;
-          return false;
-        }
-        this.game.gameOver = true;
+  get walls() {
+    return this.obstacles;
+  }
+
+  getMoveSpeed() {
+    return this.game.speed + 120;
+  }
+
+  createObstacle() {
+    return new Obstacle(this.boss.x, this.game.groundY, 30, 50);
+  }
+
+  onObstaclePassed() {}
+
+  handleCollision(w) {
+    if (isColliding(this.game.player, w)) {
+      if (this.game.player.shieldActive) {
+        this.game.player.x += 20;
+        this.coins.push({
+          x: w.x + w.width / 2,
+          y: w.y - w.height / 2,
+          vy: -120,
+          life: 0.5
+        });
+        this.game.coins++;
         return false;
       }
-      return true;
-    });
+      this.game.gameOver = true;
+      return false;
+    }
+    return true;
+  }
+
+  update(delta) {
+    const move = this.getMoveSpeed() * delta;
+    if (!this.bossFlee) {
+      super.updateObstacles(delta);
+    } else {
+      this.obstacles.forEach(w => w.update(move));
+      this.obstacles = this.obstacles.filter(w => w.x + w.width > 0);
+    }
 
     this.coins.forEach(c => {
       c.x -= move;
