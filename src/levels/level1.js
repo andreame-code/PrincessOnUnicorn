970d294dd2308525e9d6f32ed89fcab40e6fac9f
andreame-code <228015774+andreame-code@users.noreply.github.com>
2025-08-24 21:52:31 +0200

    Increase tree speed and adjust spacing

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,3 +1,8 @@
 import { BaseLevel } from './baseLevel.js';
 
-export class Level1 extends BaseLevel {}
+export class Level1 extends BaseLevel {
+  getMoveSpeed() {
+    // Increase tree velocity so the princess can pass them more easily.
+    return this.game.speed + 60;
+  }
+}
