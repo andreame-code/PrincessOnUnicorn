aa83af4e6298fd9f83ecb1c8f3d26baee424984a
andreame-code <228015774+andreame-code@users.noreply.github.com>
2025-08-29 11:00:45 +0200

    Add mobile controls for level 3

--- src/texts.js
+++ src/texts.js
@@ -1,7 +1,7 @@
 export const INSTRUCTIONS_TEXT = {
   1: 'Salta gli ostacoli premendo la barra spaziatrice o toccando lo schermo.',
   2: 'Attiva lo scudo per rompere i muri del Cavaliere Nero premendo la barra spaziatrice o toccando lo schermo.',
-  3: 'Affronta il sentiero incantato di Unicornolandia evitando i minicactus con la barra spaziatrice o un tocco sullo schermo.'
+  3: 'Affronta il sentiero incantato di Unicornolandia: usa le frecce o i pulsanti sullo schermo per muoverti e salta con la barra spaziatrice o il pulsante centrale.'
 };
 
 export const STORY_TEXT = {
